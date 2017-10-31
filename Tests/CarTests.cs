5253d828e5686a1a5f0f5859bdf89e9a32ec9112
robertnaum <robert@kellyreg.com>
2017-10-31 12:05:06 -0400

    initial Tests commit

---
+++ Tests/CarTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using CarFactory.Common;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void TestFordFocus()
+        {
+            FordAssemblyPlant fap = 
+                new FordAssemblyPlant(new GeoCoordinate(22, 44));
+
+          //  Car fordFocus = fap.CreateFocus();
+          /*
+            ModelSpecification ms;
+            ms.Model = "Focus";
+            ms.VIN = "293949030393";
+            ms.MPGCity = 22;
+            ms.MPGHwy = 29;
+            ms.WeightLBS = 2900;
+            */
+            Task<FordFocus> fordFocus = fap.NewCarAsync<FordFocus>();
+            
+        }
+    }
+}
